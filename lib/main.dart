dbe25301f8065d2f88ff9275d635a652c707a1be
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-03-13 20:07:58 +0100

    finished basic functioning interface

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
-    final player = AudioCache();
+  final player = AudioCache();
 
+  void playSound(int soundNumber) {
     player.play("note$soundNumber.wav");
   }
 
@@ -15,111 +15,118 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.red)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
                   playSound(1);
                 },
                 child: null,
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.orange)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
                   playSound(2);
                 },
                 child: null,
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.yellow)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
                   playSound(3);
                 },
                 child: null,
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.green)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
                   playSound(4);
                 },
                 child: null,
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.teal,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.teal)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
                   playSound(5);
                 },
                 child: null,
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.blue)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
                   playSound(6);
                 },
                 child: null,
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.purple,
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-              width: 100,
-              height: 40,
-              margin: EdgeInsets.only(bottom: 12),
-              child: TextButton(
+            Expanded(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>((Colors.purple)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    )),
                 onPressed: () {
-                  final player = AudioCache();
-
-                  player.play("note7.wav");
+                  playSound(7);
                 },
                 child: null,
               ),
